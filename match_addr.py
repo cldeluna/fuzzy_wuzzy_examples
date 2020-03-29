34f5824e5286e818e12b4911296dfb74b9c30ab7
Claudia de Luna <claudia@indigowire.net>
2020-03-29 07:20:29 -0700

    Initial Commit

---
+++ match_addr.py
@@ -0,0 +1,267 @@
+#!/usr/bin/python -tt
+# Project: fuzzy_wuzzy_examples
+# Filename: match_addr.py
+# claudia
+# PyCharm
+
+__author__ = "Claudia de Luna (claudia@indigowire.net)"
+__version__ = ": 1.0 $"
+__date__ = "3/21/20"
+__copyright__ = "Copyright (c) 2018 Claudia"
+__license__ = "Python"
+
+import argparse
+import pandas as pd
+import re
+import os
+from fuzzywuzzy import fuzz, process
+
+
+def df_from_excel(path):
+    """
+    Create Pandas Data Frame from given Excel file
+    :param path:
+    :return:  Pandas Data Frame from Excel file
+    """
+
+    try:
+        data_frame = pd.read_excel(path)
+
+    except IOError:
+        exit(f"ERROR!!! Failed to read Excel file: \n\t{path}\nABORTING PROGRAM Execution."
+             f"\nConfirm a valid file and correct path have been provided.")
+
+    # Return the data frame object created from the Excel file
+    return(data_frame)
+
+
+def series_compare(series1, series2, s1_desc="Series 1", s2_desc="Series 2", stdout_details=False):
+    """
+    Compare two Pandas Series
+    :param series1:  First Pandas Series
+    :param series2:  Second Pandas Series with additional details
+    :param s1_desc:  Description of first series
+    :param s2_desc:  Description of second series
+    :param stdout_details:  Print additional debug information to stdout
+    :return:
+    """
+
+    print(f"\n\t---------------------------------------------------------------")
+    print(f"\n\t--------------   SERIES COMPARE FUNCTION   --------------------")
+    print(f"\n\t---------------------------------------------------------------")
+    print(f"\t--- Pandas {s1_desc} \n{series1}\n")
+    print(f"\t--- Pandas {s2_desc} \n{series2}\n")
+
+    for s in series1:
+        print(f"\n\t---------------------------------------------------------------")
+        print(f"\t{s}")
+        # Set a boolean to determine if a match was found withing the loops
+        # Used to print a final message
+        match = False
+        for d in series2:
+            print(f"\tComparing {s1_desc} {s} with {s2_desc} {d}")
+            if re.search(s, d, re.IGNORECASE):
+                print(f"\tExact Match Found for {s1_desc} {s} in {s2_desc} {d}!")
+                match = True
+                # You found a match, break out of the for loop
+                break
+
+            # Assuming and exact match failed, proceed with the basic fuzzy wuzzy comparisons
+            ratio = fuzz.ratio(s.lower(), d.lower())
+            pratio = fuzz.partial_ratio(s.lower(), d.lower())
+            token_sort_ratio = fuzz.token_sort_ratio(s.lower(), d.lower())
+            if stdout_details:
+                print(f"\t\tFuzzy Ratio: \t{ratio}")
+                print(f"\t\tFuzzy Partial Ratio: \t{pratio}")
+                print(f"\t\tFuzzy Token Sort Ratio: \t{token_sort_ratio} of type {type(token_sort_ratio)}")
+
+            if token_sort_ratio > 55:
+                print(f"\n\tFuzzy Match Found for source: \n\t\t{s} \n\twith detail:\n\t\t{d} \n\twith Fuzzy Token Sort Ratio of {token_sort_ratio}!")
+                match = True
+                # You found a match, break out of the for loop
+                break
+
+        if not match: print(f"\n\tNo match found for {s}")
+
+    print(f"\n\t---------------------------------------------------------------")
+    print(f"\n\t---------------------------------------------------------------")
+
+
+def get_series_match(row_val, args=[]):
+
+    match_value = ''
+    stdout_details = False
+    # print(f"args is {args} \n len of args is {len(args)}")
+
+    for s in args:
+        if stdout_details:
+            print(f"\tComparing row value {row_val} with series value {s}")
+        if re.search(s, row_val , re.IGNORECASE):
+            if stdout_details:
+                print(f"\tExact Match Found for row value {row_val} with series value {s}!")
+            match_value = s
+            # You found a match, break out of the for loop
+            break
+
+        # Assuming and exact match failed, proceed with the basic fuzzy wuzzy comparisons
+        ratio = fuzz.ratio(s.lower(), row_val.lower())
+        pratio = fuzz.partial_ratio(s.lower(), row_val.lower())
+        token_sort_ratio = fuzz.token_sort_ratio(s.lower(), row_val.lower())
+        if stdout_details:
+            print(f"\t\tFuzzy Ratio: \t{ratio}")
+            print(f"\t\tFuzzy Partial Ratio: \t{pratio}")
+            print(f"\t\tFuzzy Token Sort Ratio: \t{token_sort_ratio} of type {type(token_sort_ratio)}")
+
+        if token_sort_ratio >= 50:
+            if stdout_details:
+                print(f"\n\tFuzzy Match Found for row: \n\t\t{row_val} \n\twith series value:\n\t\t{s} \n\twith Fuzzy Token Sort Ratio of {token_sort_ratio}!")
+            match_value = s
+            # You found a match, break out of the for loop
+            break
+
+    if not match_value:
+        print(f"\n\tNo match found for {s}")
+    else:
+        print(match_value)
+
+    return match_value
+
+
+# def get_values(row_val, **kwargs):
+
+#     data = kwargs['kwargs']
+
+#     print("+++++++++++++++++++++++++++++++++")
+#     # print(f"In get value function showing passed arguments \n{val} \n{kwargs} ")
+#     # print(f"\nval: \n{val}\n\n")
+#     # print(f"\n0: \n{data['addr']}")
+#     # print(f"\n1: \n{data['frame']['Address']}")
+#     # print(f"\n2: \n{data['frame'][data['col']]}")
+#     # print(f"\n3: \n{data['col']}")
+#     # print()
+
+#     match_value = ''
+#     stdout_details = True
+#     index = 0
+#     # print(f"args is {args} \n len of args is {len(args)}")
+
+#     for s in data['frame']['Address']:
+#         val = ''
+#         print(f"\tComparing row value {row_val} with series value {s}")
+#         if re.search(s, row_val , re.IGNORECASE):
+#             print(f"\tExact Match Found for row value {row_val} with series value {s}!")
+#             print(f"\t{data['frame'][data['col']]}")
+#             match_value = s
+#             val = data['frame'].loc[index, data['col']]
+#             index += 1
+#             # You found a match, break out of the for loop
+#             break
+
+#         # Assuming and exact match failed, proceed with the basic fuzzy wuzzy comparisons
+#         ratio = fuzz.ratio(s.lower(), row_val.lower())
+#         pratio = fuzz.partial_ratio(s.lower(), row_val.lower())
+#         token_sort_ratio = fuzz.token_sort_ratio(s.lower(), row_val.lower())
+#         if stdout_details:
+#             print(f"\t\tFuzzy Ratio: \t{ratio}")
+#             print(f"\t\tFuzzy Partial Ratio: \t{pratio}")
+#             print(f"\t\tFuzzy Token Sort Ratio: \t{token_sort_ratio} of type {type(token_sort_ratio)}")
+
+#         if token_sort_ratio >= 50:
+#             print(f"\n\tFuzzy Match Found for row: \n\t\t{row_val} \n\twith series value:\n\t\t{s} \n\twith Fuzzy Token Sort Ratio of {token_sort_ratio}!")
+#             match_value = s
+#             val = data['frame'].loc[index,data['col']]
+#             index += 1
+#             # You found a match, break out of the for loop
+#             break
+
+#     if not match_value:
+#         print(f"\n\tNo match found for {s}")
+#     else:
+#         print(match_value)
+#         print(f"Value: {val}")
+
+#     return match_value
+
+
+
+
+def main():
+
+    # Create a Data Frame from the Source Excel File
+    df_src = df_from_excel(arguments.source_file)
+    # Create a Data Frame from the Additional Details Excel File
+    df_det = df_from_excel(arguments.detail_file)
+
+    # Print the resulting data frames
+    # print(f"\nSource Data Frame from {arguments.source_file}")
+    # print(df_src)
+    # print(df_src.describe())
+    #
+    # print(f"\nDetails Data Frame from {arguments.detail_file}")
+    # print(df_det)
+    # print(df_det.describe())
+
+    # Series Comparison
+    # Extract Address columns (Series) for comparison
+    # Extract the Address Column of each data frame into its own Series for comparison
+    src_addr = df_src["Address"]
+    det_addr = df_det["Address"]
+    # Call Series Comparison Function
+    # series_compare(src_addr, det_addr, s1_desc="Source DSN Data", s2_desc="DSN Data Addtl Details", stdout_details=True)
+
+    # series_compare(df_src["Complex"], df_det["Complex Name"], s1_desc="Source DSN Data", s2_desc="DSN Data Addtl Details", stdout_details=True)
+
+
+
+    # Data Frame Comparison
+    # print()
+    # # print(df_src.head())
+    # for series in df_src.itertuples():
+    #     print(series)
+    #     print(series[3])
+    #     # print(df_src['Address'])
+    #     for element in df_src['Address']:
+    #         print(element)
+    #
+    # pd.set_option('display.max_columns', None)
+
+    df_src['Full_Address'] = df_src['Address'].apply(get_series_match, args=[df_det['Address']])
+    # print(df_src)
+
+    # df_src['Complex Name'] = df_src['Address'].apply(get_values, kwargs={"addr": df_det['Address'], "frame": df_det, "col":'Complex Name'})
+
+    # df_src['Complex'] = df_src['Full_Address'].map(get_series_match(series=df_det['Address']))
+    # print(df_src)
+    # print()
+    # print(df_src['Full_Address'])
+    # print(df_det)
+
+    #
+    df_merged = pd.merge(df_src, df_det[['Address','Complex Name','Postal Code', 'State_Province', 'URL']], left_on="Full_Address", right_on="Address", how="left")
+    #
+    # pd.set_option('display.max_columns', None)
+    #
+    print()
+    print(df_merged[["Complex Name", "Full_Address", "URL"]])
+
+
+    # result = pd.concat([df_src,df_det], join='outer', ignore_index=True, sort=False)
+    #
+    # print()
+    # print(result)
+
+
+# Standard call to the main() function.
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Script Description",
+                                     epilog="Usage: ' python lookup_details.py' ")
+
+    #parser.add_argument('all', help='Execute all exercises in week 4 assignment')
+    parser.add_argument('-s', '--source_file', help='Source Excel File',
+                        action='store',
+                        default="DSN_Complex_List.xlsx")
+    parser.add_argument('-d', '--detail_file', help='Excel File with additional details needed in Source Data',
+                        action='store',
+                        default="DSN_Complex_Details.xlsx")
+    arguments = parser.parse_args()
+    main()
